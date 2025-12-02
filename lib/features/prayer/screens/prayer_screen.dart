5bc5512f9e4574330d292c76c5ae4d21b69d1ec0
nime <nimeullahacse@gmail.com>
2025-12-02 22:29:20 +0600

    no changes

--- lib/features/prayer/screens/prayer_screen.dart
+++ lib/features/prayer/screens/prayer_screen.dart
@@ -21,6 +21,7 @@ class _PrayerScreenState extends State<PrayerScreen> {
         child: Column(
           children: [
             Text("A Morning Prayer for Gratitude",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 24),),
+
             SizedBox(height: 16,),
 
             Text("A Morning Prayer for Gratitude Heavenly Father, I come before you this morning with a heart full of gratitude. Thank you for the gift of a new day, for the breath in my lungs, and for the countless blessings you have bestowed upon me. Help me to see your goodness in all things, big and small. Guide my steps today, fill me with your peace, and allow me to be a reflection of your love to others. May I walk in your light and bring glory to your name. In Jesus' name, Amen.",style: TextStyle(fontSize: 16,color: Color.fromRGBO(0, 0, 0, 1)),),
